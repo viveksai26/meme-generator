af41fbd6d9fec6a0d34479257670154580ec9c42
viveksai26 <viveksai26@gmail.com>
2021-07-29 18:29:30 +0530

    Canvas fix

--- src/modules/features/public/photo-viewer/photo-viewer.component.ts
+++ src/modules/features/public/photo-viewer/photo-viewer.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { loadImage } from 'canvas';
+import { Canvas, loadImage } from 'canvas';
 @Component({
   selector: 'app-photo-viewer',
   templateUrl: './photo-viewer.component.html',
@@ -9,7 +9,7 @@ import { loadImage } from 'canvas';
 })
 export class PhotoViewerComponent implements OnInit {
   @ViewChild('canvasEl')
-  canvasEl!: ElementRef;
+  canvasEl!: ElementRef<Canvas>;
   context: any;
   textForm!: FormGroup;
   image: any;
@@ -27,42 +27,65 @@ export class PhotoViewerComponent implements OnInit {
     this.textForm.valueChanges.subscribe(() => {
       // Clear the rectangle on every change
       this.context.fillStyle = '#000000';
-      this.context.fillRect(0, 0, this.canvasEl.nativeElement.width, 20);
-      this.context.fillRect(0, this.canvasEl.nativeElement.height - 20, this.canvasEl.nativeElement.width, 20);
+      this.context.fillRect(0, 0, this.canvasEl.nativeElement.width, 60);
+      this.context.fillRect(0, this.canvasEl.nativeElement.height - 60, this.canvasEl.nativeElement.width, 60);
 
       // Text style
-      this.context.font = '20px Arial';
+      this.context.font = '30px Arial';
       this.context.textBaseline = 'middle';
       this.context.textAlign = 'center';
       this.context.fillStyle = '#ff0000';
 
       if (this.textForm.value.upperText) {
-        this.context.fillText(this.textForm.value.upperText, this.canvasEl.nativeElement.width / 2, 10);
+        this.context.fillText(this.textForm.value.upperText, this.canvasEl.nativeElement.width / 2, 30);
       }
       if (this.textForm.value.LowerText) {
         this.context.fillText(
           this.textForm.value.LowerText,
           this.canvasEl.nativeElement.width / 2,
-          this.canvasEl.nativeElement.height - 10
+          this.canvasEl.nativeElement.height - 30
         );
       }
     });
   }
   ngAfterViewInit() {
-    this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
+    this.context = (this.canvasEl.nativeElement).getContext('2d');
     this.context.imageSmoothingEnabled = false;
-    console.log(this.canvasEl.nativeElement.width);
     loadImage(this.data.image.asset).then((image) => {
       console.log(image);
+      console.log('Image width : ' + image.width);
+      console.log('Image height : ' + image.height);
       this.draw(image);
     });
   }
   draw(image: any) {
-    this.context.fillRect(0, 0, this.canvasEl.nativeElement.width, 20);
-    this.context.drawImage(image, 0, 20, this.canvasEl.nativeElement.width, this.canvasEl.nativeElement.height - 40);
-    this.context.fillRect(0, this.canvasEl.nativeElement.height - 20, this.canvasEl.nativeElement.width, 20);
+    this.context.fillRect(0, 0, this.canvasEl.nativeElement.width, 60);
+    this.context.drawImage(image, 0, 60, this.canvasEl.nativeElement.width, this.canvasEl.nativeElement.height - 120);
+    this.context.fillRect(0, this.canvasEl.nativeElement.height - 60, this.canvasEl.nativeElement.width, 60);
   }
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+  downloadImage(fileName: string = 'myimg.png') {
+    // get image data and transform mime type to application/octet-stream
+    let canvas: any = document.getElementById("myCanvas");
+    var canvasDataUrl = canvas.toDataURL()
+            .replace(/^data:image\/[^;]*/, 'data:application/octet-stream'),
+        link = document.createElement('a'); // create an anchor tag
+
+    // set parameters for downloading
+    link.setAttribute('href', canvasDataUrl);
+    link.setAttribute('target', '_blank');
+    link.setAttribute('download', fileName);
+
+    // compat mode for dispatching click on your anchor
+    if (document.createEvent) {
+        var evtObj = document.createEvent('MouseEvents');
+        evtObj.initEvent('click', true, true);
+        link.dispatchEvent(evtObj);
+    } else if (link.click) {
+        link.click();
+    }
+}
 }
